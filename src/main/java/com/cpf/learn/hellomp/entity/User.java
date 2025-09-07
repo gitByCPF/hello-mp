60f251ebc0ee8884cc3839cdd59d1c74e94e2549
cpf <cpfprogrammer@163.com>
2025-09-08 01:23:50 +0800

    1.pom中排除了lombok导致测试失败 2.测试完成

--- src/main/java/com/cpf/learn/hellomp/entity/User.java
+++ src/main/java/com/cpf/learn/hellomp/entity/User.java
@@ -4,7 +4,7 @@ import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
-import lombok.Data;
+import lombok.*;
 
 /**
  * @author cui pengfei
@@ -12,6 +12,8 @@ import lombok.Data;
  * Date 2025/9/3
  * Description: 数据库表对应的实体类
  */
+@NoArgsConstructor
+@AllArgsConstructor
 @Data
 @TableName("user")
 public class User {
--- src/test/java/com/cpf/learn/hellomp/mapper/UserMapperTest.java
+++ src/test/java/com/cpf/learn/hellomp/mapper/UserMapperTest.java
@@ -4,6 +4,7 @@ import com.cpf.learn.hellomp.entity.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -16,6 +17,18 @@ class UserMapperTest {
     public void testList(){
         List<User> users = userMapper.selectList(null);
         users.forEach(System.out::println);
+    }
+
+    @Transactional
+    @Test
+    public void testInsert(){
+        User user = new User();
+        user.setName("Alice");
+        user.setAge(19);
+        user.setEmail("test6@Alice.com");
+
+        int succeed = userMapper.insert(user);
+        System.out.println("插入成功的记录数: " + succeed);
     }
 
 }
