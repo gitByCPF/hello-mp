af044793b4e160a0a59bb9b5efd7dddb03fad7b6
cpf <cpfprogrammer@163.com>
2025-09-10 02:55:01 +0800

    分页插件的使用。

---
+++ src/main/java/com/cpf/learn/hellomp/config/MybatisPlusConfiguration.java
@@ -0,0 +1,25 @@
+package com.cpf.learn.hellomp.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author cui pengfei
+ * @since 17
+ * Date 2025/9/9
+ * Description: 配置各种插件
+ */
+@Configuration
+public class MybatisPlusConfiguration {
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        // 添加分页拦截器
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+}
